511e1c599002414fdaf84e70d8c9ac1d36b1aa69
JordiVargasRuiz <jordi.vargas3669@alumnos.udg.mx>
2025-03-30 14:29:38 -0600

    Update main.py

--- backend_ensayo/main.py
+++ backend_ensayo/main.py
@@ -52,4 +52,6 @@ def generar_ensayo():
 
 # Iniciar el servidor Flask
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 10000))
+    print(f"🚀 Servidor corriendo en http://0.0.0.0:{port}")
+    app.run(host="0.0.0.0", port=port)
